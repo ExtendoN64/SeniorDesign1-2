4b838e53d22b9369e25ef05f7edb5ffc4422ba7a
gene0nline <161842938+gene0nline@users.noreply.github.com>
2026-02-05 01:27:32 -0500

    Update and rename ESP32_S3 mapped xbox controller to ESP32_S3_mapped_xbox_controller_V0.ino

---
+++ ESP32_S3_mapped_xbox_controller_V0.ino
@@ -0,0 +1,328 @@
+//still need to add more motors and need to set up for mecanum wheels.
+
+#include <Bluepad32.h>
+#include <ESP32Sero.h>
+
+//PIN CONNECTIONS
+
+int ledPin = 2;
+int ENApin = 14; // motor 1 speed
+int IN1pin = 27; // motor 1 dir1
+int IN2pin = 27; // motor 1 dir2
+int IN3pin = 27; // motor 2 dir1
+int IN4pin = 27; // motor 2 dir2
+int ENBpin = 14; // motor 2 speed
+int xServoPin = 12;
+int yServoPin = 13;
+
+Servo xServo;
+Servo yServo;
+
+ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+
+// This callback gets called any time a new gamepad is connected.
+// Up to 4 gamepads can be connected at the same time.
+void onConnectedController(ControllerPtr ctl) {
+  bool foundEmptySlot = false;
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == nullptr) {
+      Serial.printf("CALLBACK: Controller is connected, index=%d\n", i);
+      // Additionally, you can get certain gamepad properties like:
+      // Model, VID, PID, BTAddr, flags, etc.
+      ControllerProperties properties = ctl->getProperties();
+      Serial.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id, properties.product_id);
+      myControllers[i] = ctl;
+      foundEmptySlot = true;
+      break;
+      }
+    }
+
+    if (!foundEmptySlot) {
+      Serial.println("CALLBACK: Controller connected, but could not found empty slot");
+    }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+  bool foundController = false;
+
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == ctl) {
+      Serial.printf("CALLBACK: Controller disconnected from index=%d\n", i);
+      myControllers[i] = nullptr;
+      foundController = true;
+      break;
+    }
+  }
+
+    if (!foundController) {
+      Serial.println("CALLBACK: Controller disconnected, but not found in myControllers");
+    }
+}
+
+// ========= SEE CONTROLLER VALUES IN SERIAL MONITOR ========= //
+
+void dumpGamepad(ControllerPtr ctl) {
+  Serial.printf(
+  "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
+  "misc: 0x%02x, gyro x:%6d y:%6d z:%6d, accel x:%6d y:%6d z:%6d\n",
+  ctl->index(),        // Controller Index
+  ctl->dpad(),         // D-pad
+  ctl->buttons(),      // bitmask of pressed buttons
+  ctl->axisX(),        // (-511 - 512) left X Axis
+  ctl->axisY(),        // (-511 - 512) left Y axis
+  ctl->axisRX(),       // (-511 - 512) right X axis
+  ctl->axisRY(),       // (-511 - 512) right Y axis
+  ctl->brake(),        // (0 - 1023): brake button
+  ctl->throttle(),     // (0 - 1023): throttle (AKA gas) button
+  ctl->miscButtons(),  // bitmask of pressed "misc" buttons
+  ctl->gyroX(),        // Gyro X
+  ctl->gyroY(),        // Gyro Y
+  ctl->gyroZ(),        // Gyro Z
+  ctl->accelX(),       // Accelerometer X
+  ctl->accelY(),       // Accelerometer Y
+  ctl->accelZ()        // Accelerometer Z
+  );
+}
+
+// ========= GAME CONTROLLER ACTIONS SECTION ========= //
+
+void processGamepad(ControllerPtr ctl) {
+  // There are different ways to query whether a button is pressed.
+  // By query each button individually:
+  //  a(), b(), x(), y(), l1(), etc...
+ 
+  //== XBOX A button = 0x0001 ==//
+  if (ctl->buttons() == 0x0001) {
+    // code for when A button is pushed
+    digitalWrite(ledPin, HIGH);
+  }
+  if (ctl->buttons() != 0x0001) {
+    // code for when A button is released
+    digitalWrite(ledPin, LOW);
+  }
+
+  //== XBOX B button = 0x0002 ==//
+  if (ctl->buttons() == 0x0002) {
+    // code for when B button is pushed
+  }
+  if (ctl->buttons() != 0x0002) {
+    // code for when B button is released
+  }
+
+  //== XBOX X button = 0x0004 ==//
+  if (ctl->buttons() == 0x0004) {
+    // code for when X button is pushed
+  }
+  if (ctl->buttons() != 0x0004) {
+  // code for when X button is released
+  }
+
+  //== XBOX Y button = 0x0008 ==//
+  if (ctl->buttons() == 0x0008) {
+    // code for when Y button is pushed
+  }
+  if (ctl->buttons() != 0x0008) {
+    // code for when Y button is released
+  }
+
+  //== XBOX Dpad UP button = 0x01 ==//
+  if (ctl->buttons() == 0x01) {
+    // code for when dpad up button is pushed
+  }
+  if (ctl->buttons() != 0x01) {
+    // code for when dpad up button is released
+  }
+
+  //==XBOX Dpad DOWN button = 0x02==//
+  if (ctl->buttons() == 0x02) {
+    // code for when dpad down button is pushed
+  }
+  if (ctl->buttons() != 0x02) {
+    // code for when dpad down button is released
+  }
+
+  //== XBOX Dpad LEFT button = 0x08 ==//
+  if (ctl->buttons() == 0x08) {
+    // code for when dpad left button is pushed
+  }
+  if (ctl->buttons() != 0x08) {
+    // code for when dpad left button is released
+  }
+
+  //== XBOX Dpad RIGHT button = 0x04 ==//
+  if (ctl->buttons() == 0x04) {
+    // code for when dpad right button is pushed
+  }
+  if (ctl->buttons() != 0x04) {
+    // code for when dpad right button is released
+  }
+
+  //== XBOX R1 trigger button = 0x0020 ==//
+  if (ctl->buttons() == 0x0020) {
+    // code for when R1 button is pushed
+  }
+  if (ctl->buttons() != 0x0020) {
+    // code for when R1 button is released
+  }
+
+  //== XBOX R2 trigger button = 0x0080 ==//
+  if (ctl->buttons() == 0x0080) {
+    // code for when R2 button is pushed
+  }
+  if (ctl->buttons() != 0x0080) {
+    // code for when R2 button is released
+  }
+
+  //== XBOX L1 trigger button = 0x0010 ==//
+  if (ctl->buttons() == 0x0010) {
+    // code for when L1 button is pushed
+  }
+  if (ctl->buttons() != 0x0010) {
+    // code for when L1 button is released
+  }
+
+  //== XBOX L2 trigger button = 0x0040 ==//
+  if (ctl->buttons() == 0x0040) {
+    // code for when L2 button is pushed
+  }
+  if (ctl->buttons() != 0x0040) {
+    // code for when L2 button is released
+  }
+
+  //== LEFT JOYSTICK - UP ==//  Foward movement!
+  if (ctl->axisY() <= -25) {
+    // code for when left joystick is pushed up
+    int motorspeed = map(ctl->axisY(), -25, -512, 70, 255);
+    digitalWrite(IN1pin, HIGH);
+    digitalWrite(IN2pin, LOW);
+    digitalWrite(IN3pin, HIGH);
+    digitalWrite(IN4pin, LOW);
+    digitalWrite(ENApin, motorSpeed);
+    digitalWrite(ENBpin, motorSpeed);
+    }
+
+  //== LEFT JOYSTICK - DOWN ==// Reverse!!
+  if (ctl->axisY() >= 25) {
+    // code for when left joystick is pushed down
+    int motorspeed = map(ctl->axisY(), 25, 511, 70, 255);
+    digitalWrite(IN1pin, LOW);
+    digitalWrite(IN2pin, HIGH);
+    digitalWrite(IN3pin, LOW);
+    digitalWrite(IN4pin, HIGH);
+    digitalWrite(ENApin, motorSpeed);
+    digitalWrite(ENBpin, motorSpeed);
+  }
+
+  //== LEFT JOYSTICK - LEFT ==//  Strafe Left
+  if (ctl->axisX() <= -25) {
+    // code for when left joystick is pushed left
+    int motorspeed = map(ctl->axisX(), -25, -512, 70, 255);
+    digitalWrite(IN1pin, LOW);
+    digitalWrite(IN2pin, LOW);
+    digitalWrite(IN3pin, HIGH);
+    digitalWrite(IN4pin, LOW);
+    digitalWrite(ENApin, 0);
+    digitalWrite(ENBpin, motorSpeed);
+  }
+
+  //== LEFT JOYSTICK - RIGHT ==//  Strafe Right
+  if (ctl->axisX() >= 25) {
+    // code for when left joystick is pushed right
+    int motorspeed = map(ctl->axisX(), 25, 511, 70, 255);
+    digitalWrite(IN1pin, HIGH);
+    digitalWrite(IN2pin, LOW);
+    digitalWrite(IN3pin, LOW);
+    digitalWrite(IN4pin, LOW);
+    digitalWrite(ENApin, motorspeed);
+    digitalWrite(ENBpin, 0);
+  }
+
+  //== LEFT JOYSTICK DEADZONE ==//
+  if (ctl->axisY() > -25 && ctl->axisY() < 25 && ctl->axisX() > -25 && ctl->axisX() < 25) {
+    // code for when left joystick is at idle
+    analogWrite(ENApin, 0);
+    analogWrite(ENBpin, 0);
+  }
+
+  //== RIGHT JOYSTICK - X AXIS ==//  Servo Arm!
+  if (ctl->axisRX()) {
+    // code for when right joystick moves along x-axis
+    int servoPos = map(axisRX(), -512, 511, 0, 180);
+    xServo.write(servoPos);
+  }
+
+  //== RIGHT JOYSTICK - Y AXIS ==//
+  if (ctl->axisRY()) {
+  // code for when right joystick moves along y-axis
+    int servoPos = map(axisRY(), -512, 511, 0, 180);
+    xServo.write(servoPos);
+  }
+  dumpGamepad(ctl);
+}
+
+void processControllers() {
+  for (auto myController : myControllers) {
+    if (myController && myController->isConnected() && myController->hasData()) {
+      if (myController->isGamepad()) {
+         processGamepad(myController);
+      }
+      else {
+        Serial.println("Unsupported controller");
+      }
+    }
+  }
+}
+
+// Arduino setup function. Runs in CPU 1
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(IN1pin, OUTPUT);
+  pinMode(IN2pin, OUTPUT);
+  pinMode(ENApin, OUTPUT);
+  pinMode(IN3pin, OUTPUT);
+  pinMode(IN4pin, OUTPUT);
+  pinMode(ENBpin, OUTPUT);
+
+  xServo.attach(xServoPin);
+  yServo.attach(yServoPin);
+
+  Serial.begin(115200);
+  Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+  const uint8_t* addr = BP32.localBdAddress();
+  Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+  // Setup the Bluepad32 callbacks
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+
+  // "forgetBluetoothKeys()" should be called when the user performs
+  // a "device factory reset", or similar.
+  // Calling "forgetBluetoothKeys" in setup() just as an example.
+  // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+  // But it might also fix some connection / re-connection issues.
+  BP32.forgetBluetoothKeys();
+
+  // Enables mouse / touchpad support for gamepads that support them.
+  // When enabled, controllers like DualSense and DualShock4 generate two connected devices:
+  // - First one: the gamepad
+  // - Second one, which is a "virtual device", is a mouse.
+  // By default, it is disabled.
+  BP32.enableVirtualDevice(false);
+}
+
+// Arduino loop function. Runs in CPU 1.
+void loop() {
+  // This call fetches all the controllers' data.
+  // Call this function in your main loop.
+  bool dataUpdated = BP32.update();
+  if (dataUpdated)
+    processControllers();
+
+    // The main loop must have some kind of "yield to lower priority task" event.
+    // Otherwise, the watchdog will get triggered.
+    // If your main loop doesn't have one, just add a simple `vTaskDelay(1)`.
+    // Detailed info here:
+    // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
+
+    // vTaskDelay(1);
+  delay(150);
+}
